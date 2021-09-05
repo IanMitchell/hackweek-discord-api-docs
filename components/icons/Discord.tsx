b8adb1437a6be7c1999d241b57eab0871ce38a88
Coding <dev.tandpfun@gmail.com>
2021-09-04 17:41:15 -0700

    Import logo classes correctly for mobile

--- components/icons/Discord.tsx
+++ components/icons/Discord.tsx
@@ -1,8 +1,8 @@
-export default function Discord(props: JSX.IntrinsicElements["div"]) {
+export default function Discord(props: JSX.IntrinsicElements["img"]) {
   return (
     <div>
-      <img src="/docs/images/discord_text_white.png" className="w-9/12 dark:block hidden" onMouseEnter={e => (e.target as HTMLImageElement).src = "/docs/images/discord_text_white.gif"} onMouseLeave={e => (e.target as HTMLImageElement).src = "/docs/images/discord_text_white.png"}/>
-      <img src="/docs/images/discord_text_black.png" className="w-9/12 dark:hidden" onMouseEnter={e => (e.target as HTMLImageElement).src = "/docs/images/discord_text_black.gif"} onMouseLeave={e => (e.target as HTMLImageElement).src = "/docs/images/discord_text_black.png"}/>
+      <img src="/docs/images/discord_text_white.png" className={`dark:block hidden ${props.className}`} onMouseEnter={e => (e.target as HTMLImageElement).src = "/docs/images/discord_text_white.gif"} onMouseLeave={e => (e.target as HTMLImageElement).src = "/docs/images/discord_text_white.png"}/>
+      <img src="/docs/images/discord_text_black.png" className={`dark:hidden ${props.className}`} onMouseEnter={e => (e.target as HTMLImageElement).src = "/docs/images/discord_text_black.gif"} onMouseLeave={e => (e.target as HTMLImageElement).src = "/docs/images/discord_text_black.png"}/>
     </div>
   );
 }
